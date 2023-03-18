31aeb828862cabf08dd558e54b79271e6f4f07f1
Moroz Stanislav <108823770+Morozik17@users.noreply.github.com>
2023-03-18 12:08:59 +0100

    dz

---
+++ dz.js
@@ -0,0 +1,68 @@
+// Задача №1
+
+// Напиши код, який отримає масив і використовує цикл for,
+
+// який для кожного елемента масиву буде виводити в консоль повідомлення у форматі:
+
+// /* < номер елемента > - <значення елемента >. */
+
+//Нумерація елементів повинна починатися з 1.
+
+// Наприклад для першого елемента масива['Mango', 'Poly', 'Ajax']
+
+// з індексом 0 буде виведено  1 - Mango,
+
+// а для індекса 2 выведет 3 - Ajax.
+
+// const users = ["Mango", "Poly", "Ajax"];
+
+// for (let i = 1; i < users.length+1; i++){
+//     console.log(i, users[i-1]);
+// }
+
+// ......................
+
+// Задача №2
+
+// Визначення типу значення, оператор typeof
+
+// const productName = 'Mango';
+
+// const pricePerItem = 150;
+
+// const isOnSale = false;
+
+// const error = null;
+
+// let quantity;
+
+// console.log(typeof productName);
+
+// console.log(typeof pricePerItem);
+
+// console.log(typeof isOnSale);
+
+// console.log(typeof error);
+
+// console.log(typeof quantity);
+
+
+// .....................
+
+// Задача №3
+
+// Шаблонні строки
+
+// // Написати фразу за допомогою шаблонних строк Привіт, меня  звати < імя >, мні  < стільки-то >
+
+// років і мені подобається < хоббі >, де дані з < ..... > - змінні вставлені в строку.
+
+// const name = 'Stanislav';
+
+// const age = 14;
+
+// const hobby = 'IT';
+
+// const message = `Привіт, мене звати ${name}, мені ${age} років і мені подобається ${hobby}`;
+
+// console.log(message);
